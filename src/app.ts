8a46cb18091008411cfceb3d7dce10b4d82be995
Marina Araújo <marinaaraujo@MacBook-Air-de-Marina-2.local>
2023-05-24 10:44:44 -0300

    second commit

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.post('/chat/send', async (req,resp)=>{
     }
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () =>{
     console.log(`O servidor está rodando na porta: ${port}`)
